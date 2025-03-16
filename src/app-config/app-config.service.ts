efd11c06df581a96815597943519d6853ec89f3c
Leonardo <leomeireles55@hotmail.com>
2025-03-16 18:53:01 -0300

    Add default values for configuration settings in AppConfigService

--- src/app-config/app-config.service.ts
+++ src/app-config/app-config.service.ts
@@ -6,55 +6,66 @@ export class AppConfigService {
   constructor(private readonly config: ConfigService) {}
 
   get nodeEnv(): string {
-    return this.config.get<string>('NODE_ENV');
+    return this.config.get<string>('NODE_ENV') ?? 'development';
   }
 
   get appPort(): number {
-    return this.config.get<number>('APP_PORT');
+    return this.config.get<number>('APP_PORT') ?? 3000;
   }
 
   get dbHost(): string {
-    return this.config.get<string>('DB_HOST');
+    return this.config.get<string>('DB_HOST') ?? 'localhost';
   }
 
   get dbPort(): number {
-    return this.config.get<number>('DB_PORT');
+    return this.config.get<number>('DB_PORT') ?? 5432;
   }
 
   get dbUsername(): string {
-    return this.config.get<string>('DB_USERNAME');
+    return this.config.get<string>('DB_USERNAME') ?? 'postgres';
   }
 
   get dbPassword(): string {
-    return this.config.get<string>('DB_PASSWORD');
+    return this.config.get<string>('DB_PASSWORD') ?? 'postgres';
   }
 
   get dbDatabase(): string {
-    return this.config.get<string>('DB_DATABASE');
+    return this.config.get<string>('DB_DATABASE') ?? 'soroteca_db';
   }
 
   get dbDatabaseTest(): string {
-    return this.config.get<string>('DB_DATABASE_TEST');
+    return this.config.get<string>('DB_DATABASE_TEST') ?? 'soroteca_test';
   }
 
   get emailHost(): string {
-    return this.config.get<string>('EMAIL_HOST');
+    return this.config.get<string>('EMAIL_HOST') ?? 'smtp.example.com';
   }
 
   get emailUser(): string {
-    return this.config.get<string>('EMAIL_USER');
+    return this.config.get<string>('EMAIL_USER') ?? 'user@example.com';
   }
 
   get emailPass(): string {
-    return this.config.get<string>('EMAIL_PASS');
+    return this.config.get<string>('EMAIL_PASS') ?? 'password';
   }
 
   get jwtExpiresIn(): number {
-    return this.config.get<number>('JWT_EXPIRES_IN');
+    return this.config.get<number>('JWT_EXPIRES_IN') ?? 3600;
   }
 
   get jwtSecret(): string {
-    console.log();
-    return this.config.get<string>('JWT_SECRET');
+    return this.config.get<string>('JWT_SECRET') ?? 'secret';
+  }
+  
+  get sqliteDatabase(): string {
+    return this.config.get<string>('SQLITE_DATABASE') ?? 'soroteca.db';
+  }
+  
+  get sqliteDatabaseTest(): string {
+    return this.config.get<string>('SQLITE_DATABASE_TEST') ?? 'soroteca-test.db';
+  }
+  
+  get sqliteInMemory(): boolean {
+    return this.config.get<boolean>('SQLITE_IN_MEMORY') ?? false;
   }
 }
